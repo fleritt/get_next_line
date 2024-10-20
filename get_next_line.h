c6593017a2c0f88ab2e3769696440185ef1ae38b
fleritt <rfleritt@gmail.com>
2024-10-20 19:19:10 +0200

    Add files via upload

---
+++ get_next_line.h
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rfleritt <rfleritt@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/10 17:54:08 by rfleritt          #+#    #+#             */
+/*   Updated: 2024/10/20 18:48:55 by rfleritt         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+# include <stdlib.h>
+# include <string.h>
+# include <stdio.h>
+# include <unistd.h>
+# include <fcntl.h>
+# include <stdint.h>
+
+char	*get_next_line(int fd);
+int		ft_strlen(const char *s);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strjoin(char const *s1, char const *s2);
+void	*ft_calloc(size_t num, size_t size);
+
+#endif
---
+++ get_next_line_utils.c
@@ -0,0 +1,72 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rfleritt <rfleritt@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/10 17:50:59 by rfleritt          #+#    #+#             */
+/*   Updated: 2024/10/20 18:17:55 by rfleritt         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+int	ft_strlen(char const *s)
+{
+	int	i;
+
+	i = 0;
+	while (s[i] != 0)
+		i++;
+	return (i);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*ptr;
+	size_t	i;
+	size_t	j;
+	size_t	ns;
+
+	i = 0;
+	ns = ft_strlen(s1) + ft_strlen(s2);
+	ptr = malloc(sizeof(char) * (ns + 1));
+	if (!ptr || !s1 || !s2)
+		return (NULL);
+	while (s1[i] != 0)
+	{
+		ptr[i] = s1[i];
+		i++;
+	}
+	j = 0;
+	while (s2[j] != 0)
+	{
+		ptr[i] = s2[j];
+		i++;
+		j++;
+	}
+	ptr[ns] = '\0';
+	return (ptr);
+}
+
+void	*ft_calloc(size_t num, size_t size)
+{
+	char	*ptr;
+	size_t	ns;
+	size_t	i;
+
+	i = 0;
+	if (num != 0 && size >= SIZE_MAX / num)
+		return (NULL);
+	ns = num * size;
+	ptr = (char *)malloc(ns);
+	if (ptr == NULL)
+		return (NULL);
+	while (i < ns)
+	{
+		ptr[i] = 0;
+		i++;
+	}
+	return (ptr);
+}
